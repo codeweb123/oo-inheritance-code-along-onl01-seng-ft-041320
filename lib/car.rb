4392f8f83d6166f2191a76d335a544943a100203
Jiyon FL <jiyonj@hotmail.com>
2020-04-28 22:24:07 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,19 +1,7 @@
 require_relative "./vehicle.rb"
 
 class Car < Vehicle
-
-  attr_accessor :wheel_size, :wheel_number
-
-def initialize(wheel_size, wheel_number)
-  @wheel_size = wheel_size
-  @wheel_number = wheel_number
-end
-
-def go
-  return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-end
-
-def fill_up_tank
-  return 'filling up!'
-end
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
 end
