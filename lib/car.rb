7f36cbb2b6fa4407417ce0debe4fb67543d82cc1
Jiyon FL <jiyonj@hotmail.com>
2020-04-28 22:14:20 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,17 +2,11 @@ require_relative "./vehicle.rb"
 
 class Car
 
+  attr_accessor :wheel_size, :number
+
 def initialize(wheel_size, number)
   @wheel_size = wheel_size
   @number = number
-end
-
-def wheel_size
-  @wheel_size
-end
-
-def wheel_number
-  @wheel_number
 end
 
 def go
