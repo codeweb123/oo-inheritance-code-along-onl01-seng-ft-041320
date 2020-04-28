5de3dea58c9828e227ad0a6750fe47d17ee5ada4
Jiyon FL <jiyonj@hotmail.com>
2020-04-28 22:11:35 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -6,3 +6,19 @@ def initialize(wheel_size, number)
   @wheel_size = wheel_size
   @number = number
 end
+
+def wheel_size
+  @wheel_size
+end
+
+def wheel_number
+  @wheel_number
+end
+
+def go
+  return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+end
+
+def fill_up_tank
+  return 'filling up!'
+end
